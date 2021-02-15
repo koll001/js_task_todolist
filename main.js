2228edf93700c081d68ecb11a63060b076440004
koll001 <oasis3150@gmail.com>
2021-02-15 11:46:50 +0900

    レビュー頂いた内容の修正

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 const input = document.querySelector('#js-inputTodo');
 const todoList = document.querySelector('#js-list');
-var id = 1;
 
-function createTodo(inputValue) {
+const todos = [];
+
+function displayTodos(todos) {
 	const listTable = document.createElement('tr');
 	const listId = document.createElement('td');
 	const list = document.createElement('td');
 	const stateButton = document.createElement('td');
 	const deleteButton = document.createElement('td');
 
-	listId.innerText = id++;
-	stateButton.innerHTML = `<button type="button" id="stateChange">作業中</button>`;
-	deleteButton.innerHTML = `<button type="button" id="delete">削除</button>`;
+	for (i = 0; i < todos.length; i++) {
+		listId.innerText = todos[i].id;
+		stateButton.innerHTML = `<button type="button" id="stateChange">${todos[i].status}</button>`;
+		deleteButton.innerHTML = `<button type="button" id="delete">削除</button>`;
 
-	list.innerText = inputValue;
-	listTable.append(listId, list, stateButton, deleteButton);
-	todoList.appendChild(listTable);
+		list.innerText = todos[i].task;
+		listTable.append(listId, list, stateButton, deleteButton);
+		todoList.appendChild(listTable);
+	}
+}
+
+function createTodo(inputValue) {
+	let id = 1;
+	const todo = {
+		id: id++,
+		task: inputValue,
+		status: '作業中',
+	};
+	todos.push(todo);
+	displayTodos(todos);
 }
 
 function addTodo() {
