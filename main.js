35e5a7827ac418070f87f4624a74e99f17aac418
koll001 <oasis3150@gmail.com>
2021-02-20 11:59:18 +0900

    課題3-4実装

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const input = document.getElementById('js-inputTodo');
 const todoList = document.getElementById('js-list');
 const addTodo = document.getElementById('js-addTodo');
+const radioButtons = document.getElementsByName('selectStatus');
 
 const todos = [];
+
+for (const radioButton of radioButtons) {
+	radioButton.addEventListener('change', () => {
+		switchStatus();
+	});
+}
+
+const switchStatus = () => {
+	const statusAll = document.getElementById('all');
+	const statusWorking = document.getElementById('working');
+	const statusComplete = document.getElementById('complete');
+
+	if (statusAll.checked) {
+		displayTodos(todos);
+	} else if (statusWorking.checked) {
+		const workingTodo = todos.filter((todo) => {
+			return todo.status === '作業中';
+		});
+		displayTodos(workingTodo);
+	} else if (statusComplete.checked) {
+		const completeTodo = todos.filter((todo) => {
+			return todo.status === '完了';
+		});
+		displayTodos(completeTodo);
+	}
+};
 
 const createStateButton = (todo) => {
 	const statusButton = document.createElement('button');
@@ -15,6 +42,7 @@ const createStateButton = (todo) => {
 			todo.status = '作業中';
 			statusButton.innerText = todo.status;
 		}
+		switchStatus();
 	});
 	return statusButton;
 };
@@ -30,7 +58,7 @@ const createDeleteButton = (id) => {
 		todos.map((todo, index) => {
 			todo.id = index;
 		});
-		displayTodos(todos);
+		switchStatus();
 	});
 	return deleteButton;
 };
@@ -47,18 +75,20 @@ const deleteTableRow = () => {
 const displayTodos = (todos) => {
 	deleteTableRow();
 
-	todos.map((todo) => {
-		let newCell = [];
-		let newRow = todoList.insertRow(-1);
-		for (let i = 0; i < 4; i++) {
-			newCell[i] = newRow.insertCell(i);
-		}
+	if (todos.length) {
+		todos.map((todo) => {
+			let newCell = [];
+			let newRow = todoList.insertRow(-1);
+			for (let i = 0; i < 4; i++) {
+				newCell[i] = newRow.insertCell(i);
+			}
 
-		newCell[0].innerText = todo.id;
-		newCell[1].innerText = todo.task;
-		newCell[2].appendChild(createStateButton(todo));
-		newCell[3].appendChild(createDeleteButton(todo.id));
-	});
+			newCell[0].innerText = todo.id;
+			newCell[1].innerText = todo.task;
+			newCell[2].appendChild(createStateButton(todo));
+			newCell[3].appendChild(createDeleteButton(todo.id));
+		});
+	}
 };
 
 const createTodo = (inputValue) => {
@@ -68,7 +98,7 @@ const createTodo = (inputValue) => {
 		status: '作業中',
 	};
 	todos.push(todo);
-	displayTodos(todos);
+	switchStatus();
 };
 
 addTodo.addEventListener('click', () => {
